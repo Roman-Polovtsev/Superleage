1991c1ca508c6bd50638c77000eaf7be00c4791c
Roman-Polovtsev <polovtsev96@yandex.ru>
2020-11-27 10:27:36 +0300

    loaded logger from properties file, need to try from xml, also need to delete other xml files

--- src/com/company/domain/ResultTest.java
+++ src/com/company/domain/ResultTest.java
@@ -1,28 +1,39 @@
 package com.company.domain;
 
-
-
-import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.log4j.PropertyConfigurator;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
 
 public class ResultTest {
 
     //add logger library as jar;
     //replace system.out to logger.info / warning / ...
-
+    private final static Logger logger = LoggerFactory.getLogger(ResultTest.class);
 
    // private final static Logger logger = Logger.getLogger("Our logger");
 
 
     public static void main(String[] args) {
+        Properties props = new Properties();
 
-        Logger logger = LoggerFactory.getLogger(ResultTest.class);
+        try{props.load(new FileInputStream("C:\\Users\\Public\\Documents\\log4j.xml"));}
+        catch (Exception exception){
+            System.out.println("exception");
+        }
+        PropertyConfigurator.configure(props);
 
-       // BasicConfigurator.configure();
-        PropertyConfigurator.configure("log4j2.properties");
+        String log4jConfPath = "C:\\\\Users\\\\Роман\\\\IdeaProjects\\\\Superleage\\\\src\\\\com\\\\company\\\\domain\\\\log4j.xml";
+        PropertyConfigurator.configure(log4jConfPath);
+
+       // PropertyConfigurator.configure("C:\\Users\\Роман\\IdeaProjects\\Superleage\\src\\com\\company\\domain\\resources\\log4j.xml");
         logger.info("кулебяка");
+        logger.debug("debug");
+        logger.error("error");
+        logger.trace("trace");
         Result start = new Result();
         start = start.incrementGuestPoint();
         System.out.println(start);
