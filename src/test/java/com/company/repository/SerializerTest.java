b494bc1c09265e3a94d60b2e1a9b151955df61e6
Roman-Polovtsev <polovtsev96@yandex.ru>
2021-01-10 17:49:23 +0300

    Serializing test success

--- src/test/java/com/company/repository/SerializerTest.java
+++ src/test/java/com/company/repository/SerializerTest.java
@@ -7,34 +7,39 @@ import org.mockito.Mockito;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class SerializerTest {
 
-    private Object actual;
-
     @Test
     public void serializeTest() throws IOException {
-//        Serializer serializer = new Serializer();
-//        Player [] players = {new Player()};
-//        List<Player> playerList = Arrays.asList(players);
-////        ByteArrayOutputStream byteArrayOutputStream = Mockito.mock(ByteArrayOutputStream.class);
-////        ObjectOutputStream objectOutputStream = Mockito.mock(ObjectOutputStream.class);
-//
-//        byte[] actual = serializer.serialize(playerList);
-//
-//
-//        assertArrayEquals(playerList,actual);
+        Serializer serializer = new Serializer();
+        Player player1 =  new Player("vasya");
+        Player player2 =  new Player("vasya");
+
+        byte[] serialize1 = serializer.serialize(player1);
+        byte[] serialize2 = serializer.serialize(player2);
+
+        assertArrayEquals(serialize1,serialize2);
     }
 
 
     @Test
-    public void deserializeTest() {
+    public void deserializeTest() throws IOException, ClassNotFoundException {
+        Serializer serializer = new Serializer();
+        byte[] first = {(byte)1,(byte) 2};
+        byte[] second = {(byte)1,(byte) 2};
 
+        Object deserialize = serializer.deserialize(first);
+        Object deserialize1 = serializer.deserialize(second);
+
+        assertEquals(deserialize,deserialize1);
     }
 }
